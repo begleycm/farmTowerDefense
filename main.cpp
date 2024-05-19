87d430b00defc6ca9680a32b0e379e91431beca6
Connor <cbegley027@gmail.com>
2024-05-19 16:46:10 -0400

    merge

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@
 #include <vector>
 
 int main( int argc, char* args[] ) {
-<<<<<<< HEAD
-=======
   std::cout << "start window" << std::endl;
->>>>>>> 6f79228cea26dd91b206fcc3daba68823370b354
   //Initialize SDL
   if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
   {
